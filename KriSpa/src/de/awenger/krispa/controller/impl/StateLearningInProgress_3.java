c4cc13cb27992d5e7a2f9f05ed2930e9904fe118
adrianwenger <adrianwenger@users.noreply.github.com>
2015-03-30 12:59:27 +0200

    Create StateLearningInProgress_3.java

---
+++ KriSpa/src/de/awenger/krispa/controller/impl/StateLearningInProgress_3.java
@@ -0,0 +1,41 @@
+package de.awenger.krispa.controller.impl;
+
+import de.awenger.krispa.controller.IKriSpaController;
+import de.awenger.krispa.controller.ILearningSessionState;
+
+
+/**
+ *
+ * @author Adrian Wenger
+ */
+    public final class StateLearningInProgress_3 implements ILearningSessionState {
+
+    /**
+     * KriSpa Controller.
+     */
+    private final IKriSpaController controller;
+
+    /**
+     * Public Constructor.
+     *
+     * @param cont KriSpaController
+     */
+    public StateStartLearningSession(final IKriSpaController cont) {
+        this.controller = cont;
+    }
+
+    /**
+     * now other State available.
+     */
+    @Override
+    public void change() {
+		
+        // change state to StateLearningInProgress_4
+        this.controller.setCurrentState(new StateLearningInProgress_4(controller));
+		
+		// passes map to allocateVoc
+		this.controller.allocateVoc(this.controller.divideDic(3));
+        //this.controller.getCurrentState().change();
+    }
+
+}
